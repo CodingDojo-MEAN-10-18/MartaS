f15df83d6645db27ff6a719ef42029c536c8083c
Marta Searles <marta.rose@gmail.com>
2018-10-16 11:17:12 -0700

    added hunting github

--- 01-mean/02-js-fundamentals/02-hunting-github.js
+++ 01-mean/02-js-fundamentals/02-hunting-github.js
@@ -1,1 +1,11 @@
 // 1 hour
+
+$(document).ready(function(){
+    var get = $.get("https://api.github.com/users/mroses", displayName)
+    function displayName(data) {
+        console.log(data.name)
+        $('#name_button').click(function() {
+            $('#myname').append(data.name);
+        });
+    };
+});
