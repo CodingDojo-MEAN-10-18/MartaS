e22717e06edc9536e0312e561f97b75e68ea76e9
Marta Searles <35746455+mroses@users.noreply.github.com>
2018-10-16 11:18:06 -0700

    Update 02-hunting-github.js

--- 01-mean/02-js-fundamentals/02-hunting-github.js
+++ 01-mean/02-js-fundamentals/02-hunting-github.js
@@ -1,1 +1,9 @@
-See HTML file
+$(document).ready(function(){
+    var get = $.get("https://api.github.com/users/mroses", displayName)
+    function displayName(data) {
+        console.log(data.name)
+        $('#name_button').click(function() {
+            $('#myname').append(data.name);
+        });
+    };
+});
